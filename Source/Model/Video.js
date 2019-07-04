1fa50ab8f35780f8777bdb8795dd5b035c7d8614
Adam J. Leuer <aleuer@thoughtworks.com>
2019-07-04 12:18:45 -0500

    Baseline functionality fully implemented

--- Source/Model/Video.js
+++ Source/Model/Video.js
@@ -9,17 +9,26 @@ export class Video
 		this.UploadDate = uploadDate;
 	}
 
-	static CreateFromURL(url)
+	static CreateFromURL(urlObject)
 	{
-		var video = new Video(url.href, url.href, new Date(url)); //temporarily setting the title to the url until we get back the real title
+		var video = new Video(urlObject.href, urlObject.href, this.ExtractDateFromURL(urlObject)); //temporarily setting the title to the url until we get back the real title
 
 		var videoTitleUpdater = (videoTitle) =>
 		{
 			video.Title = videoTitle;
 		};
 
-		WebPageTitleLookupService.LookupWebPageTitle(url, videoTitleUpdater);
+		WebPageTitleLookupService.LookupWebPageTitle(urlObject, videoTitleUpdater);
 
 		return video;
+	}
+
+	static ExtractDateFromURL(urlObject)
+	{
+		const dateTemplate = "YYYY-MM-DD";
+		var path = urlObject.pathname;
+		var dateString = path.slice(1, (1 + dateTemplate.length));
+		const date = new Date(dateString);
+		return date;
 	}
 }
